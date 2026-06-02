dba32d0267e2a1b749183488bea699cf08f2cec2
riddhesh_misal <riddhesh.misal@talentica.com>
2026-06-03 01:02:04 +0530

    env file implementation done

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ class Config:
         self.db_host = secret_provider.get_secret("DB_HOST")
         self.db_user = secret_provider.get_secret("DB_USER")
         self.db_password = secret_provider.get_secret("DB_PASSWORD")
+        self.jwt_secret = secret_provider.get_secret("JWT_SECRET")
--- app/secret_provider.py
+++ app/secret_provider.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import os
 
 
 class SecretProvider(ABC):
@@ -6,3 +7,16 @@ class SecretProvider(ABC):
     @abstractmethod
     def get_secret(self, key: str) -> str:
         pass
+
+
+class EnvSecretProvider(SecretProvider):
+
+    def get_secret(self, key: str) -> str:
+        value = os.getenv(key)
+
+        if not value:
+            raise ValueError(
+                f"Missing environment variable: {key}"
+            )
+
+        return value
